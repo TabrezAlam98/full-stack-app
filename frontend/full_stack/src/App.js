d63f363b7b90c6c46b2a6fb0d3e051e72b79e9a7
Tabrez Alam <97460388+TabrezAlam98@users.noreply.github.com>
2023-02-16 00:02:49 +0530

    frontend

---
+++ frontend/full_stack/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Text,Box } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+      <Box>
+        <Text>This is app file</Text>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
